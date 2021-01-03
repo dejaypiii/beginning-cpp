67d2753324ffa3cbab2b3067acc7a0b127a2d4f6
_dejaypiii <dejaypiii@pm.me>
2021-01-03 20:12:45 +0100

    add first 'hello world' cpp example

---
+++ Beginning Cpp/1-Game Over/GameOver.cpp
@@ -0,0 +1,12 @@
+// Game Over 2.0
+// A first C++ programm demonstrates a using directive
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Game Over!" << endl;
+	return 0;
+}
